025e9a65a7f10989aed06ab6c00838bd398ce91d
Hungai Amuhinda <hungaiinc@gmail.com>
2023-04-03 17:01:41 +0300

    test cases

--- subprocesses/subprocesses_test.go
+++ subprocesses/subprocesses_test.go
@@ -2,7 +2,7 @@ package subprocesses_test
 
 import (
 	"context"
-	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -11,270 +11,84 @@ import (
 	"github.com/smartcontractkit/libocr/subprocesses"
 )
 
-func TestSubprocesses_Go(t *testing.T) {
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	var counter int
-
+func TestGoAndWait(t *testing.T) {
+	var counter int32
 	s := &subprocesses.Subprocesses{}
+
 	for i := 0; i < 10; i++ {
 		s.Go(func() {
-			time.Sleep(time.Millisecond)
-			mu.Lock()
-			counter++
-			mu.Unlock()
-			wg.Done()
+			atomic.AddInt32(&counter, 1)
+			time.Sleep(50 * time.Millisecond)
 		})
-		wg.Add(1)
 	}
-	wg.Wait()
 
-	assert.Equal(t, 10, counter, "Expected counter to be 10")
-}
-
-func TestSubprocesses_Wait(t *testing.T) {
-	var wg sync.WaitGroup
-	var counter int
-
-	s := &subprocesses.Subprocesses{}
-	for i := 0; i < 10; i++ {
-		s.Go(func() {
-			time.Sleep(time.Millisecond)
-			wg.Done()
-		})
-		wg.Add(1)
-	}
 	s.Wait()
-
-	assert.Equal(t, 0, counter, "Expected WaitGroupCounter to be 0")
+	assert.Equal(t, int32(10), counter)
 }
 
-func TestSubprocesses_BlockForAtMost(t *testing.T) {
+func TestBlockForAtMost(t *testing.T) {
 	s := &subprocesses.Subprocesses{}
 
-	// Test with timeout shorter than function duration
-	start := time.Now()
-	ok := s.BlockForAtMost(context.Background(), time.Millisecond*500, func(ctx context.Context) {
+	f := func(ctx context.Context) {
 		select {
-		case <-time.After(time.Second):
-			// Wait for longer than the timeout
 		case <-ctx.Done():
-			// Return when the context is cancelled
+		case <-time.After(50 * time.Millisecond):
 		}
-	})
-	elapsed := time.Since(start)
+	}
 
-	assert.False(t, ok, "Expected BlockForAtMost to return false")
-	assert.LessOrEqual(t, elapsed, time.Millisecond*700, "Expected elapsed time to be less than 700ms")
+	ctx := context.Background()
 
-	// Test with timeout longer than function duration
-	start = time.Now()
-	ok = s.BlockForAtMost(context.Background(), time.Second, func(ctx context.Context) {
+	// Test with a function that finishes before the specified duration
+	result := s.BlockForAtMost(ctx, 100*time.Millisecond, f)
+	assert.True(t, result, "Function should have finished before the specified duration")
+
+	// Test with a function that takes longer than the specified duration
+	result = s.BlockForAtMost(ctx, 25*time.Millisecond, f)
+	assert.False(t, result, "Function should not have finished before the specified duration")
+}
+
+func TestBlockForAtMostMany(t *testing.T) {
+	s := &subprocesses.Subprocesses{}
+	ctx := context.Background()
+
+	fastFunc := func(ctx context.Context) {
 		select {
-		case <-time.After(time.Millisecond * 500):
-			// Wait for shorter than the timeout
 		case <-ctx.Done():
-			// Return when the context is cancelled
+		case <-time.After(50 * time.Millisecond):
 		}
-	})
-	elapsed = time.Since(start)
-
-	assert.True(t, ok, "Expected BlockForAtMost to return true")
-	assert.GreaterOrEqual(t, elapsed, time.Millisecond*500, "Expected elapsed time to be at least 500ms")
-}
-
-func TestSubprocesses_BlockForAtMostMany(t *testing.T) {
-	s := &subprocesses.Subprocesses{}
-	fs := []func(context.Context){
-		func(ctx context.Context) {
-			select {
-			case <-time.After(time.Second):
-				// Wait for longer than the timeout
-			case <-ctx.Done():
-				// Return when the context is cancelled
-			}
-		},
-		func(ctx context.Context) {
-			select {
-			case <-time.After(time.Millisecond * 500):
-				// Wait for shorter than the timeout
-			case <-ctx.Done():
-				// Return when the context is cancelled
-			}
-		},
-		func(ctx context.Context) {
-			select {
-			case <-time.After(time.Millisecond * 750):
-				// Wait for longer than the timeout
-			case <-ctx.Done():
-				// Return when the context is cancelled
-			}
-		},
 	}
 
-	// Test with timeout shorter than longest function duration
-	start := time.Now()
-	ok, oks := s.BlockForAtMostMany(context.Background(), time.Millisecond*700, fs...)
-	elapsed := time.Since(start)
+	slowFunc := func(ctx context.Context) {
+		select {
+		case <-ctx.Done():
+		case <-time.After(200 * time.Millisecond):
+		}
+	}
 
-	assert.False(t, ok, "Expected BlockForAtMostMany to return false")
-	assert.Equal(t, []bool{false, true, false}, oks, "Expected oks to be [false, true, false]")
-	assert.LessOrEqual(t, elapsed, time.Millisecond*1000, "Expected elapsed time to be less than 1000ms")
+	// Test with multiple functions that finish before the specified duration
+	ok, results := s.BlockForAtMostMany(ctx, 100*time.Millisecond, fastFunc, fastFunc, fastFunc)
+	assert.True(t, ok, "All functions should have finished before the specified duration")
+	assert.Equal(t, []bool{true, true, true}, results)
 
-	// Test with timeout longer than longest function duration
-	start = time.Now()
-	ok, oks = s.BlockForAtMostMany(context.Background(), time.Second, fs...)
-	elapsed = time.Since(start)
-
-	assert.True(t, ok, "Expected BlockForAtMostMany to return true")
-	assert.Equal(t, []bool{false, true, true}, oks, "Expected oks to be [false, true, true]")
-	assert.GreaterOrEqual(t, elapsed, time.Millisecond*750, "Expected elapsed time to be at least 750ms")
+	// Test with a mix of functions, some finishing before the specified duration and others taking longer
+	ok, results = s.BlockForAtMostMany(ctx, 100*time.Millisecond, fastFunc, slowFunc, fastFunc)
+	assert.False(t, ok, "Not all functions should have finished before the specified duration")
+	assert.Equal(t, []bool{true, false, true}, results)
 }
 
-func TestSubprocesses_RepeatWithCancel(t *testing.T) {
-	var mu sync.Mutex
-	var counter int
-
+func TestRepeatWithCancel(t *testing.T) {
+	var counter int32
 	s := &subprocesses.Subprocesses{}
 	ctx, cancel := context.WithCancel(context.Background())
-	s.RepeatWithCancel("test", time.Millisecond*500, ctx, func() {
-		mu.Lock()
-		counter++
-		mu.Unlock()
+
+	s.RepeatWithCancel("test", 20*time.Millisecond, ctx, func() {
+		atomic.AddInt32(&counter, 1)
 	})
 
-	// Wait for a short time to allow some repetitions
-	time.Sleep(time.Millisecond * 1500)
+	time.Sleep(110 * time.Millisecond)
 	cancel()
-
-	assert.GreaterOrEqual(t, counter, 2, "Expected counter to be at least 2")
-}
-
-func TestSubprocesses_BlockForAtMost_EdgeCases(t *testing.T) {
-	s := &subprocesses.Subprocesses{}
-
-	// Test with zero duration
-	ok := s.BlockForAtMost(context.Background(), 0, func(ctx context.Context) {})
-	assert.True(t, ok, "Expected BlockForAtMost with 0 duration to return true")
-
-	// Test with negative duration
-	ok = s.BlockForAtMost(context.Background(), -1*time.Second, func(ctx context.Context) {})
-	assert.True(t, ok, "Expected BlockForAtMost with negative duration to return true")
-
-	// Test with nil function
-	ok = s.BlockForAtMost(context.Background(), time.Second, nil)
-	assert.False(t, ok, "Expected BlockForAtMost with nil function to return false")
-}
-
-func TestSubprocesses_Go_Concurrency(t *testing.T) {
-	s := &subprocesses.Subprocesses{}
-	var mu sync.Mutex
-	var counter int
-
-	// Start 100 goroutines that each increment a shared variable
-	for i := 0; i < 100; i++ {
-		s.Go(func() {
-			time.Sleep(time.Millisecond)
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		})
-	}
-
-	// Wait for all goroutines to finish
 	s.Wait()
 
-	assert.Equal(t, 100, counter, "Expected counter to be 100")
-}
-
-func TestSubprocesses_BlockForAtMostMany_Concurrency(t *testing.T) {
-	s := &subprocesses.Subprocesses{}
-	var mu sync.Mutex
-	var counter int
-	fs := []func(context.Context){
-		func(ctx context.Context) {
-			time.Sleep(time.Millisecond)
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		},
-		func(ctx context.Context) {
-			time.Sleep(time.Millisecond * 50)
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		},
-		func(ctx context.Context) {
-			time.Sleep(time.Millisecond * 100)
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		},
-	}
-
-	// Start 100 goroutines that each call BlockForAtMostMany with a slice of functions
-	for i := 0; i < 100; i++ {
-		s.Go(func() {
-			ok, _ := s.BlockForAtMostMany(context.Background(), time.Millisecond*500, fs...)
-			if ok {
-				mu.Lock()
-				counter++
-				mu.Unlock()
-			}
-		})
-	}
-
-	// Wait for all goroutines to finish
-	s.Wait()
-
-	assert.GreaterOrEqual(t, counter, 100, "Expected counter to be at least 100")
-}
-
-func TestSubprocesses_RepeatWithCancel_Cancel(t *testing.T) {
-	s := &subprocesses.Subprocesses{}
-	var mu sync.Mutex
-	var counter int
-
-	// Create a cancelled context
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-
-	// Start a goroutine that calls RepeatWithCancel with the cancelled context
-	s.Go(func() {
-		s.RepeatWithCancel("test", time.Millisecond*500, ctx, func() {
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		})
-	})
-
-	// Wait for the goroutine to finish
-	s.Wait()
-
-	assert.Equal(t, 0, counter, "Expected counter to be 0")
-}
-
-func TestSubprocesses_BlockForAtMostMany_Cancel(t *testing.T) {
-	s := &subprocesses.Subprocesses{}
-	var mu sync.Mutex
-	var counter int
-
-	// Create a cancelled context
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-
-	// Start a goroutine that calls BlockForAtMostMany with the cancelled context and a slice of functions
-	s.Go(func() {
-		ok, _ := s.BlockForAtMostMany(ctx, time.Millisecond*500, func(ctx context.Context) {
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		})
-		assert.False(t, ok, "Expected BlockForAtMostMany with cancelled context to return false")
-	})
-
-	// Wait for the goroutine to finish
-	s.Wait()
-
-	assert.Equal(t, 0, counter, "Expected counter to be 0")
+	assert.GreaterOrEqual(t, counter, int32(5))
+	assert.LessOrEqual(t, counter, int32(6))
 }
